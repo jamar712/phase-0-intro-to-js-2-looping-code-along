aa4ea1bbea271626e54e33a50f6d7a5df6c3d6a6
Jamar Graves <jamargraves07@gmail.com>
2023-08-24 16:36:45 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName) {
+  let array2 = [ ];
+  for (let i = 0; i < stringNames.length; i++) {
+    array2.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+    debugger;
+  }
+  return(array2);
+  
+}
+
+writeCards(["John", "Joe", "Jim"], "Anniversary");
+
+function countDown(n){
+  let countDown = n 
+  while (n >= 0){
+    console.log(n--);
+  }
+}
+
